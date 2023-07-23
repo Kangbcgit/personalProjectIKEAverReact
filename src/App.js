b268fa8c307118653672ec2265da33b51332d526
Kangbcgit <kangbcgit@gmail.com>
2023-07-23 10:04:38 +0900

    폴더 전환 위한 커밋

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import Header from './component/Header/Header.jsx';
-import MainBanner from './component/MainBanner/MainBanner.jsx';
-import Info from './component/Info/Info.jsx';
-import HomeFunishingIdeas from './component/HomeFunishingIdeas/HomeFunishingIdeas.jsx';
-import EventBanner from './component/EventBanner/EventBanner.jsx';
-import BestCategori from './component/BestCategori/BestCategori.jsx';
-import StoreLocation from './component/StoreLocation/StoreLocation.jsx';
-import Footer from './component/Footer/Footer.jsx';
+// import MainBanner from './component/MainBanner/MainBanner.jsx';
+// import Info from './component/Info/Info.jsx';
+// import HomeFunishingIdeas from './component/HomeFunishingIdeas/HomeFunishingIdeas.jsx';
+// import EventBanner from './component/EventBanner/EventBanner.jsx';
+// import BestCategori from './component/BestCategori/BestCategori.jsx';
+// import StoreLocation from './component/StoreLocation/StoreLocation.jsx';
+// import Footer from './component/Footer/Footer.jsx';
 function App() {
   return (
     <>
-    
+      <Header />
     </>
   );
 }
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import styles from './Header.module.scss'
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <>
+
+      </>
+    );
+  }
+}
+export default Header
